4cbc48e1ba97e387222785b7516769f935a92440
Jeff King <jazfunk@gmail.com>
2020-07-29 19:56:31 -0400

    Add project files.

---
+++ NHibernateDemoApp/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace NHibernateDemoApp
+{
+    class Student
+    {
+        public virtual int id { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string FirstMidName { get; set; }
+    }
+}
